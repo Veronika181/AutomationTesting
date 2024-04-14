849a5ecc7b3b002d8bc2cfda9e7b7acaacc1ae8f
Veronika <veronika.obrtelova181@gmail.com>
2024-04-14 17:53:00 +0200

    commit

--- pages/MainPage.py
+++ pages/MainPage.py
@@ -6,14 +6,10 @@ from datetime import datetime
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class MainPage:
     def __init__(self, driver):
         self.driver = driver
-
-    def validity_card(date):
-        date = datetime.now()
-        # self.driver.find_element(By.ID, "valid-since-input").send_keys(date)
-        print("Datum", date)
 
     def select_fuel_type(self):
         fuel_type = WebDriverWait(self.driver, 10).until(
@@ -21,35 +17,38 @@ class MainPage:
         )
         fuel_type.click()
 
-
     def select_kind_card(self):
         self.driver.find_element(By.XPATH, "//h5[contains(text(),'Roční')]").click()
+
     def validity_card(self):
-        date = datetime.today()
+        date = datetime(2024, 4, 20)
         self.driver.find_element(By.ID, "valid-since-input").send_keys(date.strftime("%d.%m.%Y"))
 
     def emails(self):
-        self.driver.find_element(By.ID, "email-input").send_keys("jannovak@seznam.cz")
+        self.driver.find_element(By.XPATH, "//input[@id='email-input']").send_keys("jannovak@seznam.cz")
 
+    def emailsrepeat(self):
+        self.driver.find_element(By.XPATH, "//input[@id='email-confirmation-input']").send_keys("jannovak@seznam.cz")
 
     def click_continue(self):
         self.driver.find_element(By.XPATH, "//span[contains(text(),'Pokračovat')]").click()
 
     def checkbox(self):
-        self.driver.find_element(By.ID, "//input[@id = 'notificationEnabled-true']").click()
+        self.driver.find_element(By.XPATH, "//input[@id ='notificationEnabled-true']").click()
 
-    def againcheckbox(self):
-        self.driver.find_element(By.ID, "custom-control-input").click()
+    def emailcheckconfirm(self):
+        self.driver.find_element(By.XPATH, "//input[@id='_isNotificationEmailSame-true']").click()
 
     def mobilenumber(self):
-        self.driver.find_element(By.XPATH, "kit__input-phone").click()
+        self.driver.find_element(By.XPATH, "//input[@class='kit__input-phone']").send_keys("606432999")
 
     def payment(self):
-        self.driver.find_element(By. XPATH, "//h5[contains(text(),'Platební kartou')]")
-        #class ="mb-3 col-auto" > Platební kartou < / div >
+        self.driver.find_element(By.XPATH, "//input[@id='bank_transfer_payment_radio_array_option']").click()
+
+    def payButtonExist(self):
+        assert self.driver.find_element(By.XPATH, "//span[contains(text(),'Zaplatit')]")
 
     def conditions(self):
-        self.driver.find_element(By. ID, "terms-and-conditions-check").click()
+        self.driver.find_element(By.XPATH, "//input[@id='_termsAgreement-true']").click()
 
-    def cancelpayment(self):
-        self.driver.find_element(By.ID, "button -with-icon button-cancel").click()
+
--- tests/buynewcard.py
+++ tests/buynewcard.py
@@ -1,3 +1,4 @@
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -21,15 +22,19 @@ main_page.select_fuel_type()
 main_page.click_continue()
 main_page.select_kind_card()
 main_page.validity_card()
+time.sleep(2)
 main_page.click_continue()
 main_page.emails()
-main_page.emails()
-main_page.check_box()
+main_page.emailsrepeat()
+main_page.checkbox()
+main_page.emailcheckconfirm()
 main_page.mobilenumber()
 main_page.click_continue()
+main_page.payment()
 main_page.conditions()
-main_page.click_continue()
-main_page.cancelpayment()
+main_page.payButtonExist()
+
+
 
 
 
