4065626c7242cca764fc5caa2687907ac6bf1fa1
Veronika <veronika.obrtelova181@gmail.com>
2024-04-23 11:16:10 +0200

    commit

--- tests/bulk_purchase_card.py
+++ tests/bulk_purchase_card.py
@@ -20,18 +20,4 @@ batch_page.click_continue()
 batch_page.click_continue()
 
 
-#main_page.stampsvalidity
-#main_page.daystamps
-#main_page.newsetofstamps
-#main_page.continuebulkpurchase
-#main_page.againcontinue
-#login_page.email
-#login_page.email
-#login_page.checkboxnotice
-#login_page.sameemailconfirmation
-#login_page.telephonenumber
-#login_page.click_continue()
-#main_page.pay
-#main_page.agreetermspayment
-
 
